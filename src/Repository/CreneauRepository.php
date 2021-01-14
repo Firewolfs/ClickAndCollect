2d820a61007f78cddeb036581fb86eee5e96f502
Teluep <teluep@gmail.com>
2021-01-14 13:54:58 +0100

    Avancer

--- src/Repository/CreneauRepository.php
+++ src/Repository/CreneauRepository.php
@@ -19,32 +19,4 @@ class CreneauRepository extends ServiceEntityRepository
         parent::__construct($registry, Creneau::class);
     }
 
-    // /**
-    //  * @return Creneau[] Returns an array of Creneau objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Creneau
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
